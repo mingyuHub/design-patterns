56b9b38e88fe344ceb9bce8b21f347099dae8a91
陈明羽 <cmysouls@163.com>
2019-03-15 14:31:09 +0800

    代理模式

---
+++ proxy/Phoneable.java
@@ -0,0 +1,14 @@
+package com.example.design.proxy;
+
+/**
+ * @author: chenmingyu
+ * @date: 2019/3/1 10:20
+ * @description: 手机接口
+ */
+public interface Phoneable {
+
+    /**
+     * 打电话
+     */
+    void call();
+}
---
+++ proxy/ProxyTest.java
@@ -0,0 +1,14 @@
+package com.example.design.proxy;
+
+/**
+ * @author: chenmingyu
+ * @date: 2019/3/1 14:21
+ * @description:
+ */
+public class ProxyTest {
+
+    public static void main(String[] args) {
+        Proxy proxy = new Proxy();
+        proxy.call();
+    }
+}
